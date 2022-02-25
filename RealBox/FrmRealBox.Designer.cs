9d1f4361daece493dcadc2dcd77bc4256beaeba2
Sylvio Leal Giraldes <sylviogiraldes@yahoo.com>
2022-02-25 08:09:47 +0000

    Add project files.

---
+++ RealBox/FrmRealBox.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace RealBox
+{
+    partial class FrmRealBox
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRealBox1 = new System.Windows.Forms.TextBox();
+            this.txtRealBox2 = new System.Windows.Forms.TextBox();
+            this.txtRealBox3 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtRealBox1
+            // 
+            this.txtRealBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtRealBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtRealBox1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.txtRealBox1.Location = new System.Drawing.Point(311, 53);
+            this.txtRealBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtRealBox1.Name = "txtRealBox1";
+            this.txtRealBox1.Size = new System.Drawing.Size(518, 49);
+            this.txtRealBox1.TabIndex = 0;
+            this.txtRealBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtRealBox1.Enter += new System.EventHandler(this.txtRealBox_Enter);
+            this.txtRealBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtRealBox_KeyDown);
+            this.txtRealBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRealBox_KeyPress);
+            // 
+            // txtRealBox2
+            // 
+            this.txtRealBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtRealBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtRealBox2.ForeColor = System.Drawing.Color.DarkCyan;
+            this.txtRealBox2.Location = new System.Drawing.Point(313, 137);
+            this.txtRealBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtRealBox2.Name = "txtRealBox2";
+            this.txtRealBox2.Size = new System.Drawing.Size(518, 49);
+            this.txtRealBox2.TabIndex = 1;
+            this.txtRealBox2.Enter += new System.EventHandler(this.txtRealBox_Enter);
+            this.txtRealBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtRealBox_KeyDown);
+            this.txtRealBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRealBox_KeyPress);
+            // 
+            // txtRealBox3
+            // 
+            this.txtRealBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtRealBox3.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtRealBox3.ForeColor = System.Drawing.Color.DarkCyan;
+            this.txtRealBox3.Location = new System.Drawing.Point(313, 220);
+            this.txtRealBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtRealBox3.Name = "txtRealBox3";
+            this.txtRealBox3.Size = new System.Drawing.Size(518, 49);
+            this.txtRealBox3.TabIndex = 2;
+            this.txtRealBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtRealBox3.Enter += new System.EventHandler(this.txtRealBox_Enter);
+            this.txtRealBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtRealBox_KeyDown);
+            this.txtRealBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRealBox_KeyPress);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(37, 343);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1068, 417);
+            this.panel1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(34, 107);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 40);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Function Keys";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(291, 350);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(437, 31);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "ARROWS       - Move cursor to left or right";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(291, 303);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(501, 31);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "MINUS           - Flips negative and posiive values";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(291, 257);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(436, 31);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "BACKSPACE   - Delete first digit to the left";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(291, 210);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(440, 31);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "DELETE          - Delete first digit to the left ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(291, 163);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(512, 31);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "END               - Move cursor to rightmost position";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(291, 117);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "HOME            - Clear text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(34, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 55);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "RealBox";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(291, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(367, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Only digits will be accepted";
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lblMsg.ForeColor = System.Drawing.Color.Red;
+            this.lblMsg.Location = new System.Drawing.Point(320, 302);
+            this.lblMsg.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(511, 25);
+            this.lblMsg.TabIndex = 4;
+            this.lblMsg.Click += new System.EventHandler(this.lblMsg_Click);
+            this.lblMsg.DoubleClick += new System.EventHandler(this.lblMsg_Click);
+            // 
+            // FrmRealBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1143, 783);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtRealBox3);
+            this.Controls.Add(this.txtRealBox2);
+            this.Controls.Add(this.txtRealBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmRealBox";
+            this.Text = "RealBox - Captures Numeric Fields in Value Format";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRealBox1;
+        private System.Windows.Forms.TextBox txtRealBox2;
+        private System.Windows.Forms.TextBox txtRealBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ RealBox/FrmRealBox.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RealBox
+{
+    public partial class FrmRealBox : Form
+    {
+        public FrmRealBox() => InitializeComponent();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //  Supply initial values. Use format to avoid Culture issues such as comma instead of decimal point
+            txtRealBox1.Text = string.Format("{0:#,##0.00}", 12.34);
+            txtRealBox2.Text = string.Format("{0:#,##0.00}", 0.0);
+            txtRealBox3.Text = 0.ToString("N2"); // Another way to do the same thing
+        }
+        private void txtRealBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) || e.KeyChar == 45)
+            {
+                TextBox t = (TextBox)sender;
+                int cursorPosition = t.Text.Length - t.SelectionStart;      // Text in the box and Cursor position
+
+                if (e.KeyChar == 45)                    
+                    t.Text = t.Text[0] == 45 ? t.Text = t.Text[1..] : "-" + t.Text;                    
+                else                    
+                    if ( t.Text.Length < 20)
+                        t.Text = ( decimal.Parse( t.Text.Insert( t.SelectionStart, e.KeyChar.ToString())
+                                                .Replace(",", "").Replace(".", "")) / 100).ToString("N2");
+
+                t.SelectionStart = (t.Text.Length - cursorPosition < 0 ? 0 : t.Text.Length - cursorPosition);
+            }
+            e.Handled = true;
+        }
+        private void txtRealBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back || e.KeyCode == Keys.Delete)     // Deals with BackSpace e Delete keys
+            {
+                TextBox t = (TextBox)sender;
+                int cursorPosition = t.Text.Length - t.SelectionStart;
+
+                string Left  = t.Text.Substring( 0, t.Text.Length - cursorPosition).Replace(".", "").Replace(",", "");
+                string Right = t.Text.Substring( t.Text.Length - cursorPosition   ).Replace(".", "").Replace(",", "");
+
+                if (Left.Length > 0)
+                {
+                    Left = Left.Remove( Left.Length - 1);                            // Take out the rightmost digit
+                    t.Text = ( decimal.Parse( Left + Right) / 100).ToString("N2");
+                    t.SelectionStart = (t.Text.Length - cursorPosition < 0 ? 0 : t.Text.Length - cursorPosition);
+                }                
+                e.Handled = true;
+            }
+
+            if (e.KeyCode == Keys.End)                                  // Treats End key
+            {
+                TextBox t = (TextBox)sender;
+                t.SelectionStart = t.Text.Length;                       // Moves the cursor o the rightmost position
+                e.Handled = true;
+            }
+
+            if (e.KeyCode == Keys.Home)                                 // Trata tecla Home
+            {
+                TextBox t = (TextBox)sender;
+                t.Text = 0.ToString("N2");                              // Set field value to zero 
+                t.SelectionStart = t.Text.Length;                       // Moves the cursor o the rightmost position
+                e.Handled = true;
+            }                
+        }
+        private void txtRealBox_Enter(object sender, EventArgs e)
+        {
+            TextBox t = (TextBox)sender;                                // Desliga seleção de texto
+            t.SelectionStart = t.Text.Length;
+        }
+        private void lblMsg_Click(object sender, EventArgs e)
+        {
+            lblMsg.Text = "";
+        }
+    }
+}
